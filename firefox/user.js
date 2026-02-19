6229bcd7c30cb1300d472d1ee7ca0c2337c04bfc
cloud30302525-lang <cloud30302525@gmail.com>
2026-02-20 06:46:41 +0700

    firefox

--- firefox/user.js
+++ firefox/user.js
@@ -3,3 +3,4 @@ user_pref("svg.context-properties.content.enabled", true);
 user_pref("full-screen-api.warning.timeout", 0);
 user_pref("ui.key.menuAccessKeyFocuses", false);
 user_pref("browser.tabs.closeWindowWithLastTab", false);
+user_pref("browser.tabs.allowTabDetach", false);
